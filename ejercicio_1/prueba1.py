722beccdf51f63e242b7d53b6284330a714d2beb
mgarmorn <mgarmorn@g.educaand.es>
2023-10-05 18:11:08 +0200

    a

--- ejercicio_1/prueba1.py
+++ ejercicio_1/prueba1.py
@@ -1,2 +1,5 @@
-edad = int(input("Dime tu edad:"))
-print("Tienes",edad,"años.")
+edad = input("Introduzca su edad: ")
+if edad >= 18:
+    print("Toma una cerveza!")
+else:
+    print(f"Toma un zumo de piña, con {edad} años eres menor.")
